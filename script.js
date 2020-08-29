08ee667417bd29dfbea27762f6dc7de529812c6c
Ali Nguyen <alin57717@gmail.com>
2020-08-29 14:09:58 -0700

    add variable to store curent date

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ let searchHistoryEl =$(".historyItems");
 let weatherIconEl = $(".weatherIcon");
 let currentDateEl = $(".currentDate");
 let cityNameEl = $(".cityName");
+
+
+let windSpeedEl = $(".windSpeed");
+let uvIndexEl = $(".windspeed");
+let cardRow = $(".card-row");
+let tempEl = $(".temp");
+let humidityEl = $(".humidity");
+
+
+var today = new Date ();
+var today = mm + '/' + dd + '/' + yyyy;
+let dd = String(today.getDate()).padstart(2,'0');
+let mm = String(today.getMonth() + 1).padstart(2.'0');
+let yyyy = today.getFullYear();
+
