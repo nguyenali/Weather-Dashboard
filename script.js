563bcc486b0f6943a41d691961d500c09d649340
Ali Nguyen <alin57717@gmail.com>
2020-08-29 14:03:46 -0700

    create variables to display current weather conditions

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let searchInput = $ (".searchInput");
 let searchBtn = $(".searchBtn");
 let apiKey = "3a"
+
+
+let searchHistoryEl =$(".historyItems");
+let weatherIconEl = $(".weatherIcon");
+let currentDateEl = $(".currentDate");
+let cityNameEl = $(".cityName");
